13699f05d1007a2ef62c8cf686bcf9549210ace9
aliwaseem27 <ali.waseem27@gmail.com>
2024-04-26 02:44:07 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/aliwaseem/personal_guide/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.aliwaseem.personal_guide
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:personal_guide/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
